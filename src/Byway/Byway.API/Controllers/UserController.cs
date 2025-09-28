0c55040676e21c78d5b17b54623783542206dae2
mohamadhesham00 <mohamadhesham112@gmail.com>
2025-09-28 15:27:04 +0300

    Feat: added register & login endpoints that returns access token

---
+++ src/Byway/Byway.API/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using Byway.Application.DTOs.User;
+using Byway.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Byway.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController(IUserService userService) : APIControllerBase
+    {
+        private readonly IUserService _userService = userService;
+
+        // POST: api/user/register
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] CreateUserDto dto)
+        {
+            var result = await _userService.RegisterAsync(dto);
+            return HandleResult(result);
+        }
+
+        // POST: api/user/login
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto dto)
+        {
+            var result = await _userService.LoginAsync(dto);
+            return HandleResult(result);
+
+        }
+
+        // GET: api/user/me
+        //[HttpGet("me")]
+        //public async Task<IActionResult> Me()
+        //{
+        //    var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        //    if (userId == null)
+        //        return Unauthorized();
+
+        //    var user = await _userManager.FindByIdAsync(userId);
+        //    if (user == null)
+        //        return NotFound();
+
+        //    return Ok(new
+        //    {
+        //        user.Id,
+        //        user.UserName,
+        //        user.Email
+        //    });
+        //}
+    }
+
+}
