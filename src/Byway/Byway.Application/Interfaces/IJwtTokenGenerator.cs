6c1b513a007e0a951ad2e7b1e837041f72946aaa
mohamadhesham00 <mohamadhesham112@gmail.com>
2025-09-28 15:20:55 +0300

    Feat: added service for generating access token

---
+++ src/Byway/Byway.Application/Interfaces/IJwtTokenGenerator.cs
@@ -0,0 +1,10 @@
+﻿using Byway.Domain.Entities;
+
+namespace Byway.Application.Interfaces
+{
+    public interface IJwtTokenGenerator
+    {
+        string GenerateToken(AppUser user, IList<string> roles);
+
+    }
+}
---
+++ src/Byway/Byway.Infrastructure/Services/JwtTokenGenerator.cs
@@ -0,0 +1,48 @@
+﻿using Byway.Application.Interfaces;
+using Byway.Domain.Entities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Byway.Infrastructure.Services
+{
+    public class JwtTokenGenerator : IJwtTokenGenerator
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtTokenGenerator(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(AppUser user, IList<string> roles)
+        {
+            var jwtSettings = _configuration.GetSection("JwtSettings");
+
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email ?? string.Empty),
+                new Claim("username", user.UserName ?? string.Empty),
+                new Claim("role",roles[0])
+            };
+
+            // Add roles
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: jwtSettings["Issuer"],
+                audience: jwtSettings["Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(Convert.ToDouble(jwtSettings["DurationInMinutes"])),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
