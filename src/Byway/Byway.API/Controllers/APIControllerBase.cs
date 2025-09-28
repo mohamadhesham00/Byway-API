11b800126e9417efd8aee9e951c6feff0b2aa139
mohamadhesham00 <mohamadhesham112@gmail.com>
2025-09-28 15:26:28 +0300

    Feat: added generic & reuseable Controller base that has method like handling result returned from services

---
+++ src/Byway/Byway.API/Controllers/APIControllerBase.cs
@@ -0,0 +1,23 @@
+﻿using Byway.Application.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Byway.API.Controllers
+{
+    [ApiController]
+    public class APIControllerBase : ControllerBase
+    {
+        protected IActionResult HandleResult<T>(Result<T> result)
+        {
+            if (result.IsSuccess)
+            {
+                return Ok(new { success = true, data = result.Value });
+            }
+
+            return StatusCode(result.Status ?? 400, new
+            {
+                success = false,
+                error = result.ErrorMessage
+            });
+        }
+    }
+}
