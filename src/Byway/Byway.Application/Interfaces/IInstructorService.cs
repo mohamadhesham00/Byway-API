68b328423801a6503898e115e4ac1ce3450881f3
mohamadhesham00 <mohamadhesham112@gmail.com>
2025-09-30 17:42:28 +0300

    Feat: added forgotten Interface & mapping class

---
+++ src/Byway/Byway.Application/Interfaces/IInstructorService.cs
@@ -0,0 +1,19 @@
+﻿using Byway.Application.Common.Models;
+using Byway.Application.DTOs;
+using Byway.Application.DTOs.Instructor;
+
+namespace Byway.Application.Interfaces
+{
+    public interface IInstructorService
+    {
+        Task<Result<InstructorDto>> AddInstructorAsync(CreateInstructorDto dto, CancellationToken cancellationToken = default);
+        public Task<Result<PagedResult<InstructorDto>>> GetInstructorsAsync(int pageNumber, int pageSize, InstructorFilter? filter, CancellationToken cancellationToken);
+        Task<Result<InstructorDto>> GetInstructorByIdAsync(Guid instructorId, CancellationToken cancellationToken = default);
+        Task<Result<InstructorDto>> UpdateInstructorAsync(Guid instructorId, UpdateInstructorDto dto, CancellationToken cancellationToken = default);
+        Task<Result> DeleteInstructorAsync(Guid instructorId, CancellationToken cancellationToken = default);
+        public Result<IEnumerable<EnumDto>> GetJobTitles();
+
+
+
+    }
+}
---
+++ src/Byway/Byway.Application/Mappings/InstructorMapping.cs
@@ -0,0 +1,32 @@
+﻿using Byway.Application.DTOs.Instructor;
+using Byway.Domain.Entities;
+
+namespace Byway.Application.Mappings
+{
+    public static class InstructorMapping
+    {
+        public static InstructorDto ToDto(this Instructor instructor) =>
+            new()
+            {
+                Id = instructor.Id,
+                Name = instructor.Name,
+                JobTitle = instructor.JobTitle,
+                Rate = instructor.Rate,
+                Description = instructor.Description,
+                ImagePath = instructor.ImagePath
+            };
+        public static Instructor ToEntity(this CreateInstructorDto dto, Guid userId) =>
+            new()
+            {
+                Id = Guid.NewGuid(),
+                Name = dto.Name,
+                ImagePath = dto.ImagePath,
+                JobTitle = dto.JobTitle,
+                Rate = dto.Rate,
+                Description = dto.Description,
+                CreatedAt = DateTime.Now,
+                CreatedBy = userId
+            };
+    }
+
+}
